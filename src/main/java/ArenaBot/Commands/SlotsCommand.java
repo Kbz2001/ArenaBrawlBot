faa56ccf0e07ec536da1036e928ff0704d60151a
Kbz <kilezisme@gmail.com>
2018-11-12 22:18:46 -0600

    Yay!

--- src/main/java/ArenaBot/Commands/SlotsCommand.java
+++ src/main/java/ArenaBot/Commands/SlotsCommand.java
@@ -9,11 +9,12 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import java.awt.*;
 import java.util.Random;
 
-public class SlotsCommand {
+public class SlotsCommand extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e)
