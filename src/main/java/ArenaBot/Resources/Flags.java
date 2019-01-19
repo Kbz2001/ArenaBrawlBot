f5aba09782ec002e4f4d652163b3ebef9692f642
Kbz <kilezisme@gmail.com>
2019-01-18 23:30:34 -0600

    Added a whole lot of features + Flag Game yay!

--- src/main/java/ArenaBot/Resources/Flags.java
+++ src/main/java/ArenaBot/Resources/Flags.java
@@ -16,34 +16,119 @@ public class Flags
 		flags.put(":flag_ae:", "United Arab Emirates");
 		flags.put(":flag_af:", "Afghanistan");
 		flags.put(":flag_ag:", "Antigua and Barbuda");
-//		flags.put(":flag_ai:", "Anguilla");
-//		flags.put(":flag_al:", "Albania");
-//		flags.put(":flag_am:", "Armenia");
-//		flags.put(":flag_ao:", "Angola");
-//		flags.put(":flag_aq:", "Antarctica");
-//		flags.put(":flag_ar:", "Argentina");
-//		flags.put(":flag_as:", "American Samoa");
-//		flags.put(":flag_at:", "Austria");
-//		flags.put(":flag_au:", "Australia");
-//		flags.put(":flag_aw:", "Aruba");
-//		flags.put(":flag_ax:", "Åland");
-//		flags.put(":flag_az:", "Azerbaijan");
+		flags.put(":flag_ai:", "Anguilla");
+		flags.put(":flag_al:", "Albania");
+		flags.put(":flag_am:", "Armenia");
+		flags.put(":flag_ao:", "Angola");
+		flags.put(":flag_aq:", "Antarctica");
+		flags.put(":flag_ar:", "Argentina");
+		flags.put(":flag_as:", "American Samoa");
+		flags.put(":flag_at:", "Austria");
+		flags.put(":flag_au:", "Australia");
+		flags.put(":flag_aw:", "Aruba");
+		flags.put(":flag_ax:", "Åland");
+		flags.put(":flag_az:", "Azerbaijan");
 
 		//B Flags
-//		flags.put(":flag_ba:", "Bosnia and Herzegovina");
-//		flags.put(":flag_bb:", "Barbados");
-//		flags.put(":flag_bd:", "Bangladesh");
-//		flags.put(":flag_be:", "Belgium");
-//		flags.put(":flag_bf:", "Burkina Faso");
-//		flags.put(":flag_bg:", "Bulgaria");
-//		flags.put(":flag_bh:", "Bahrain");
-//		flags.put(":flag_bi:", "Burundi");
-//		flags.put(":flag_bj:", "Benin");
-//		flags.put(":flag_bl:", "Saint Barthélemy");
-		//flags.put("", "");
+		flags.put(":flag_ba:", "Bosnia and Herzegovina");
+		flags.put(":flag_bb:", "Barbados");
+		flags.put(":flag_bd:", "Bangladesh");
+		flags.put(":flag_be:", "Belgium");
+		flags.put(":flag_bf:", "Burkina Faso");
+		flags.put(":flag_bg:", "Bulgaria");
+		flags.put(":flag_bh:", "Bahrain");
+		flags.put(":flag_bi:", "Burundi");
+		flags.put(":flag_bj:", "Benin");
+		flags.put(":flag_bl:", "Saint Barthélemy");
+		flags.put(":flag_bm:", "Bermuda");
+		flags.put(":flag_bn:", "Brunei");
+		flags.put(":flag_bo:", "Bolivia");
+		flags.put(":flag_bq:", "Bonaire");
+		flags.put(":flag_br:", "Brazil");
+		flags.put(":flag_bs:", "Bahamas");
+		flags.put(":flag_bt:", "Bhutan");
+		flags.put(":flag_bv:", "Bouvet Island");
+		flags.put(":flag_bw:", "Botswana");
+		flags.put(":flag_by:", "Belarus");
+		flags.put(":flag_bz:", "Belize");
 
+		//C Flags
+		flags.put(":flag_ca:", "Canada");
+		flags.put(":flag_cc:", "Cocos");
+		flags.put(":flag_cd:", "Democratic Republic of the Congo");
+		flags.put(":flag_cf:", "Central African Republic");
+		flags.put(":flag_cg:", "Republic of the Congo");
+		flags.put(":flag_ch:", "Switzerland");
+		flags.put(":flag_ci:", "Cote d'Ivoire");
+		flags.put(":flag_ck:", "Cook Islands");
+		flags.put(":flag_cl:", "Chile");
+		flags.put(":flag_cm:", "Cameroon");
+		flags.put(":flag_cn:", "China");
+		flags.put(":flag_co:", "Colombia");
+		flags.put(":flag_cr:", "Costa Rica");
+		flags.put(":flag_cu:", "Cuba");
+		flags.put(":flag_cv:", "Cabo Verde");
+		flags.put(":flag_cw:", "Curacao");
+		flags.put(":flag_cx:", "Christmas Island");
+		flags.put(":flag_cy:", "Cyprus");
+		flags.put(":flag_cz:", "Czechia");
 
+		//G Flags
+		flags.put(":flag_de:", "Germany");
+		flags.put(":flag_dg:", "Diego Garcia");
+		flags.put(":flag_dj:", "Djibouti");
+		flags.put(":flag_dk:", "Denmark");
+		flags.put(":flag_dm:", "Dominica");
+		flags.put("flag_do:", "Dominican Republic");
+		flags.put(":flag_dz:", "Algeria");
+		flags.put(":flag_ea:", "Ceuta & Melilla");
 
+		//E Flags
+		flags.put(":flag_ec:", "Ecuador");
+		flags.put(":flag_ee:", "Estonia");
+		flags.put(":flag_eg:", "Egypt");
+		flags.put(":flag_eh:", "Western Sahara");
+		flags.put(":flag_er:", "Eritrea");
+		flags.put(":flag_es:", "Spain");
+		flags.put(":flag_et:", "Ethiopia");
+		flags.put(":flag_eu:", "European Union");
+
+		//F Flags
+		flags.put(":flag_fi:", "Finland");
+		flags.put(":flag_fj:", "Fiji");
+		flags.put(":flag_fk:", "Falkland Islands");
+		flags.put(":flag_fm:", "Federated States of Micronesia");
+		flags.put(":flag_fo:", "Faroe Islands");
+		flags.put(":flag_fr:", "France");
+
+		//G Flags
+		flags.put(":flag_ga:", "Gabon");
+		flags.put(":flag_gb:", "United Kingdom");
+		flags.put(":flag_gd:", "Grenada");
+		flags.put(":flag_ge:", "Georgia");
+		flags.put(":flag_gf:", "French Guiana");
+		flags.put(":flag_gg:", "Guernsey");
+		flags.put(":flag_gh:", "Ghana");
+		flags.put(":flag_gi:", "Gibraltar");
+		flags.put(":flag_gl:", "Greenland");
+		flags.put(":flag_gm:", "The Gambia");
+		flags.put(":flag_gn:", "Guinea");
+		flags.put(":flag_gp:", "Guadeloupe");
+		flags.put(":flag_gq:", "Equatorial Guinea");
+		flags.put(":flag_gr:", "Greece");
+		flags.put(":flag_gs:", "South Georgia and the South Sandwich Islands");
+		flags.put(":flag_gt:", "Guatemala");
+		flags.put(":flag_gu:", "Guam");
+		flags.put(":flag_gw:", "Guinea-Bissau");
+		flags.put(":flag_gy:", "Guyana");
+
+		//H Flags
+		flags.put(":flag_hk:", "Hong Kong");
+		flags.put(":flag_hm:", "Heard Island and McDonald Island");
+		flags.put(":flag_hn:", "Honduras");
+		flags.put(":flag_hr:", "Croatia");
+		flags.put(":flag_ht:", "Haiti");
+		flags.put(":flag_hu:", "Hungary");
 
 		return flags;
 
