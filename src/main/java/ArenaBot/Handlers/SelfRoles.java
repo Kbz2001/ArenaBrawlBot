6ec894b1e930794ad4b37707bf9cd1505842e7b0
kbz2001 <kilezisme@gmail.com>
2020-11-02 14:19:46 -0600

    Updated Javacord version due to breaking bugs + Updated SelfRoles to work properly.

--- src/main/java/ArenaBot/Handlers/SelfRoles.java
+++ src/main/java/ArenaBot/Handlers/SelfRoles.java
@@ -1,6 +1,7 @@
 package ArenaBot.Handlers;
 
 import ArenaBot.App;
+import org.javacord.api.entity.DiscordEntity;
 import org.javacord.api.entity.channel.TextChannel;
 import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.permission.Role;
@@ -11,6 +12,8 @@ import org.javacord.api.listener.message.reaction.ReactionAddListener;
 import org.javacord.api.listener.message.reaction.ReactionRemoveListener;
 
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class SelfRoles implements ReactionAddListener, ReactionRemoveListener
 {
@@ -25,18 +28,25 @@ public class SelfRoles implements ReactionAddListener, ReactionRemoveListener
         Optional<Message> msg = e.getMessage();
         String getMsgID = String.valueOf(e.getMessageId());
         TextChannel tChannel = e.getChannel();
-        User user = e.getUser();
+        String userID = e.getUserIdAsString();
+
         String emoji = e.getEmoji().getMentionTag()
                 .replaceAll(":", "")
                 .replace("<", "")
                 .replace(">", "").replaceAll("[\\d]", "");
 
-        Optional<Role> arenaRole = App.api.getRoleById("480842311060946984");
-        Optional<Role> mcRole = App.api.getRoleById("551186530949922826");
-        Optional<Role> brawlhallaRole = App.api.getRoleById("489295469969670174");
-        Optional<Role> rauRole = App.api.getRoleById("354767956292665345");
-        Optional<Role> homeworkRole = App.api.getRoleById("551171545129549834");
-        Optional<Role> coderRole = App.api.getRoleById("344703514229866497");
+        Role arenaRole = null;
+        Role mcRole = null;
+        Role brawlhallaRole = null;
+        Role rauRole = null;
+        Role homeworkRole = null;
+        Role coderRole = null;
+        Optional<Role> op_arenaRole = App.api.getRoleById("480842311060946984");
+        Optional<Role> op_mcRole = App.api.getRoleById("551186530949922826");
+        Optional<Role> op_brawlhallaRole = App.api.getRoleById("489295469969670174");
+        Optional<Role> op_rauRole = App.api.getRoleById("354767956292665345");
+        Optional<Role> op_homeworkRole = App.api.getRoleById("551171545129549834");
+        Optional<Role> op_coderRole = App.api.getRoleById("344703514229866497");
 
         if(!App.isOnline)
         {
@@ -56,37 +66,91 @@ public class SelfRoles implements ReactionAddListener, ReactionRemoveListener
 
                     case "ArenaBrawl":
 
-                        arenaRole.ifPresent(user::addRole);
+                        if(op_arenaRole.isPresent()) {
+                            arenaRole = op_arenaRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(arenaRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "Creative":
 
-                        mcRole.ifPresent(user::addRole);
+                        if(op_mcRole.isPresent()) {
+                            mcRole = op_mcRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(mcRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "⚔":
 
-                        brawlhallaRole.ifPresent(user::addRole);
+                        if(op_brawlhallaRole.isPresent()) {
+                            brawlhallaRole = op_brawlhallaRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(brawlhallaRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "RAU":
 
-                        rauRole.ifPresent(user::addRole);
+                        if(op_rauRole.isPresent()) {
+                            rauRole = op_rauRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(rauRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "\uD83D\uDCDA":
 
-                        homeworkRole.ifPresent(user::addRole);
+                        if(op_homeworkRole.isPresent()) {
+                            homeworkRole = op_homeworkRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(homeworkRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "\uD83D\uDCBB":
 
-                        coderRole.ifPresent(user::addRole);
+                        if(op_coderRole.isPresent()) {
+                            coderRole = op_coderRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.addRole(coderRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
@@ -105,18 +169,25 @@ public class SelfRoles implements ReactionAddListener, ReactionRemoveListener
         Optional<Message> msg = e.getMessage();
         String getMsgID = String.valueOf(e.getMessageId());
         TextChannel tChannel = e.getChannel();
-        User user = e.getUser();
+        String userID = e.getUserIdAsString();
+
         String emoji = e.getEmoji().getMentionTag()
                 .replaceAll(":", "")
                 .replace("<", "")
                 .replace(">", "").replaceAll("[\\d]", "");
 
-        Optional<Role> arenaRole = App.api.getRoleById("480842311060946984");
-        Optional<Role> mcRole = App.api.getRoleById("551186530949922826");
-        Optional<Role> brawlhallaRole = App.api.getRoleById("489295469969670174");
-        Optional<Role> rauRole = App.api.getRoleById("354767956292665345");
-        Optional<Role> homeworkRole = App.api.getRoleById("551171545129549834");
-        Optional<Role> coderRole = App.api.getRoleById("344703514229866497");
+        Role arenaRole = null;
+        Role mcRole = null;
+        Role brawlhallaRole = null;
+        Role rauRole = null;
+        Role homeworkRole = null;
+        Role coderRole = null;
+        Optional<Role> op_arenaRole = App.api.getRoleById("480842311060946984");
+        Optional<Role> op_mcRole = App.api.getRoleById("551186530949922826");
+        Optional<Role> op_brawlhallaRole = App.api.getRoleById("489295469969670174");
+        Optional<Role> op_rauRole = App.api.getRoleById("354767956292665345");
+        Optional<Role> op_homeworkRole = App.api.getRoleById("551171545129549834");
+        Optional<Role> op_coderRole = App.api.getRoleById("344703514229866497");
 
         if(!App.isOnline)
         {
@@ -136,37 +207,91 @@ public class SelfRoles implements ReactionAddListener, ReactionRemoveListener
 
                     case "ArenaBrawl":
 
-                        arenaRole.ifPresent(user::addRole);
+                        if(op_arenaRole.isPresent()) {
+                            arenaRole = op_arenaRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(arenaRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "Creative":
 
-                        mcRole.ifPresent(user::addRole);
+                        if(op_mcRole.isPresent()) {
+                            mcRole = op_mcRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(mcRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "⚔":
 
-                        brawlhallaRole.ifPresent(user::addRole);
+                        if(op_brawlhallaRole.isPresent()) {
+                            brawlhallaRole = op_brawlhallaRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(brawlhallaRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "RAU":
 
-                        rauRole.ifPresent(user::addRole);
+                        if(op_rauRole.isPresent()) {
+                            rauRole = op_rauRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(rauRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "\uD83D\uDCDA":
 
-                        homeworkRole.ifPresent(user::addRole);
+                        if(op_homeworkRole.isPresent()) {
+                            homeworkRole = op_homeworkRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(homeworkRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
                     case "\uD83D\uDCBB":
 
-                        coderRole.ifPresent(user::addRole);
+                        if(op_coderRole.isPresent()) {
+                            coderRole = op_coderRole.get();
+                        }
+
+                        try {
+                            User user = App.api.getUserById(userID).get();
+                            user.removeRole(coderRole);
+                        } catch (InterruptedException | ExecutionException ex) {
+                            ex.printStackTrace();
+                        }
 
                         break;
 
